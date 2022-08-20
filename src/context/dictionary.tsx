d99aae5df4c64c55e3b96d130906473e7ed81ea4
SaulReznik <robhov97@gmail.com>
2022-08-20 19:56:17 +0400

    local storage name changed

--- src/context/dictionary.tsx
+++ src/context/dictionary.tsx
@@ -2,18 +2,18 @@ import React, { useReducer, Reducer } from 'react';
 
 import { IDictionaryContextAction, IDictionaryProvider, IDictionaryContext, IDictionary } from './types';
 
-const initialState: IDictionary = JSON.parse(localStorage.getItem('note-dictionary') || '{}') || {};
+const initialState: IDictionary = JSON.parse(localStorage.getItem('note-dictionary-list') || '{}') || {};
 
 // TODO: Refactor during the redux swith
 const reducer: Reducer<IDictionary, IDictionaryContextAction> = (state, action) => {
     switch (action.type) {
         case 'ADD_ITEM':
         case 'EDIT_ITEM':
-            localStorage.setItem('note-dictionary', JSON.stringify({ ...state, [action.payload.id]: action.payload }))
+            localStorage.setItem('note-dictionary-list', JSON.stringify({ ...state, [action.payload.id]: action.payload }))
             return { ...state, [action.payload.id]: action.payload };
         case 'DELETE_ITEM':
             delete state[action.payload.id];
-            localStorage.setItem('note-dictionary', JSON.stringify({ ...state }))
+            localStorage.setItem('note-dictionary-list', JSON.stringify({ ...state }))
             return { ...state }
         default:
             return state;
